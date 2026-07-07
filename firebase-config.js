db7480c8ab348eb902b74b232acb15cf832513da
rsguo813-dotcom <rsguo813@gmail.com>
2026-07-07 17:45:55 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+apiKey: "AIzaSyBRz3s7Dxqd3STffIK3dmyheAkRlOwceVo",
+  authDomain: "tic-tac-toe-c7933.firebaseapp.com",
+  projectId: "tic-tac-toe-c7933",
+  storageBucket: "tic-tac-toe-c7933.firebasestorage.app",
+  messagingSenderId: "985799107392",
+  appId: "1:985799107392:web:a1ea1cfc2eb9400daf50a5"
 };
