74e41d56a6da319454635788df2ec27bc35c2698
rsguo813-dotcom <rsguo813@gmail.com>
2026-07-07 17:54:00 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-apiKey: "AIzaSyBRz3s7Dxqd3STffIK3dmyheAkRlOwceVo",
+  apiKey: "AIzaSyBRz3s7Dxqd3STffIK3dmyheAkRlOwceVo",
   authDomain: "tic-tac-toe-c7933.firebaseapp.com",
+  databaseURL: "https://tic-tac-toe-c7933-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "tic-tac-toe-c7933",
   storageBucket: "tic-tac-toe-c7933.firebasestorage.app",
   messagingSenderId: "985799107392",
-  appId: "1:985799107392:web:a1ea1cfc2eb9400daf50a5"
+  appId: "1:985799107392:web:c3094d1e77c2ef6aaf50a5"
 };
